4b9faffb984c3ccaef7fd0f0ef8f97577eb77389
“Haphjpsy <phjpsy@naver.com>
2020-11-18 16:32:14 +0900

    all files

---
+++ exe1.js
@@ -0,0 +1,23 @@
+class Parent {
+    name = 'Lee';
+
+    hello() {
+        console.log('hello', this.name);
+    }
+}
+
+class Child extends Parent {
+    age = 37;
+
+    hello() {
+        console.log('hello', this.name, this.age);
+    }
+}
+
+const p = new Parent();
+const c = new Child();
+
+console.log(p, c);
+c.hello();
+c.name = 'Anna';
+c.hello();
---
+++ exe2.js
@@ -0,0 +1,32 @@
+class Parent {
+    name;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    hello() {
+        console.log('hello', this.name);
+    }
+}
+
+class Child extends Parent {
+    age;
+
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+
+    hello() {
+        console.log('hello', this.name, this.age);
+    }
+}
+
+const p = new Parent('Mark');
+const c = new Child('Mark', 37);
+
+console.log(p, c);
+c.hello();
+c.name = 'Anna';
+c.hello();
